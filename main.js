fb746150786a604ef42a754be1ea2e82b16ae5f0
patrick.schott <patrick.schott@kittelberger.de>
2024-09-16 18:20:49 +0200

    styling

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function initMap() {
   // Position der Karte (z.B. Berlin)
   var location = { lat: 52.52, lng: 13.405 };
   // Erstellung der Karte im #map Element
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: location,
   });
@@ -12,3 +12,32 @@ function initMap() {
     map: map,
   });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".fade");
+
+  const observerOptions = {
+    threshold: 0.2,
+  };
+
+  const fadeInOnScroll = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("fade-in");
+        observer.unobserve(entry.target); // Optional: Stop observing once the fade-in is applied
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(fadeInOnScroll, observerOptions);
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+});
+
+// nav
+document.addEventListener("DOMContentLoaded", () => {
+  const nav = document.querySelector("nav");
+  nav.classList.add("fade-in");
+});
