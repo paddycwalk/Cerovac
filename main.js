686a346301e018796ed2c4548158c751ab0ea284
Patrick Schott <schottpatrick@gmx.de>
2024-09-23 00:04:37 +0200

    1

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function initMap() {
   // Position der Karte (z.B. Berlin)
   var location = { lat: 52.52, lng: 13.405 };
   // Erstellung der Karte im #map Element
-  var map = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
     center: location,
   });
@@ -13,8 +13,8 @@ function initMap() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const sections = document.querySelectorAll(".fade");
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('.fade');
 
   const observerOptions = {
     threshold: 0.2,
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const fadeInOnScroll = (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("fade-in");
+        entry.target.classList.add('fade-in');
         observer.unobserve(entry.target); // Optional: Stop observing once the fade-in is applied
       }
     });
@@ -37,7 +37,40 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // nav
-document.addEventListener("DOMContentLoaded", () => {
-  const nav = document.querySelector("nav");
-  nav.classList.add("fade-in");
+// document.addEventListener('DOMContentLoaded', () => {
+//   const nav = document.querySelector('nav');
+//   nav.classList.add('fade-in');
+// });
+
+let lastScrollTop = 0;
+const nav = document.querySelector('nav');
+
+// Beim Laden der Seite wird die Klasse 'show' nach einer kleinen Verzögerung gesetzt
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    nav.classList.add('show'); // Navigation reinfaden
+  }, 100); // Kleine Verzögerung für sanfteres Laden
+});
+
+window.addEventListener('scroll', () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Runterscrollen - Navigation ausblenden
+    nav.classList.add('hide');
+    nav.classList.remove('show');
+  } else {
+    // Hochscrollen - Navigation einblenden
+    nav.classList.add('show');
+    nav.classList.remove('hide');
+  }
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Fix für mobiles Scrollen
+});
+
+// google btn
+document.querySelector('.Kontakt_btn').addEventListener('click', function () {
+  const background = document.querySelector('.Kontakt_bg');
+  background.style.opacity = '0'; // Setzt die Opazität des Hintergrundbildes auf 0
+  background.style.pointerEvents = 'none'; // Ermöglicht Interaktionen mit der Karte
+  this.style.display = 'none'; // Versteckt den Button
 });
