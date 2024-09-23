aeb93cd6ce74dbd67b87ed35d1c726aee969d6a6
patrick.schott <patrick.schott@kittelberger.de>
2024-09-23 11:43:28 +0200

    [main]

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
-function initMap() {
-  // Position der Karte (z.B. Berlin)
-  var location = { lat: 52.52, lng: 13.405 };
-  // Erstellung der Karte im #map Element
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: location,
-  });
-  // Marker auf die Karte setzen
-  var marker = new google.maps.Marker({
-    position: location,
-    map: map,
-  });
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('.fade');
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".fade");
 
   const observerOptions = {
     threshold: 0.2,
@@ -23,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const fadeInOnScroll = (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('fade-in');
+        entry.target.classList.add("fade-in");
         observer.unobserve(entry.target); // Optional: Stop observing once the fade-in is applied
       }
     });
@@ -36,41 +21,35 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// nav
-// document.addEventListener('DOMContentLoaded', () => {
-//   const nav = document.querySelector('nav');
-//   nav.classList.add('fade-in');
-// });
-
 let lastScrollTop = 0;
-const nav = document.querySelector('nav');
+const nav = document.querySelector("nav");
 
 // Beim Laden der Seite wird die Klasse 'show' nach einer kleinen Verzögerung gesetzt
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   setTimeout(() => {
-    nav.classList.add('show'); // Navigation reinfaden
+    nav.classList.add("show"); // Navigation reinfaden
   }, 100); // Kleine Verzögerung für sanfteres Laden
 });
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop > lastScrollTop) {
     // Runterscrollen - Navigation ausblenden
-    nav.classList.add('hide');
-    nav.classList.remove('show');
+    nav.classList.add("hide");
+    nav.classList.remove("show");
   } else {
     // Hochscrollen - Navigation einblenden
-    nav.classList.add('show');
-    nav.classList.remove('hide');
+    nav.classList.add("show");
+    nav.classList.remove("hide");
   }
   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Fix für mobiles Scrollen
 });
 
 // google btn
-document.querySelector('.Kontakt_btn').addEventListener('click', function () {
-  const background = document.querySelector('.Kontakt_bg');
-  background.style.opacity = '0'; // Setzt die Opazität des Hintergrundbildes auf 0
-  background.style.pointerEvents = 'none'; // Ermöglicht Interaktionen mit der Karte
-  this.style.display = 'none'; // Versteckt den Button
+document.querySelector(".Kontakt_btn").addEventListener("click", function () {
+  const background = document.querySelector(".Kontakt_bg");
+  background.style.opacity = "0";
+  background.style.pointerEvents = "none";
+  this.style.display = "none";
 });
