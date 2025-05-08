523eacc6a38cef562341318eea48161526c90779
patrick.schott <patrick.schott@kittelberger.de>
2025-05-08 16:07:57 +0200

    update 2.0

--- dist/_res/scripts/main.js
+++ dist/_res/scripts/main.js
@@ -1,31 +1,31 @@
 // global variables
-const burger = document.querySelector('.Burger');
-const nav = document.querySelector('nav');
-const kontaktMap = document.querySelector('.Kontakt_map');
-const kontaktBtn = document.querySelector('.Kontakt_btn');
-const kontaktBg = document.querySelector('.Kontakt_bg');
-const counter = document.querySelector('.Ueberuns_counter');
-const badge = document.querySelector('.Ueberuns_badge');
+const burger = document.querySelector(".Burger");
+const nav = document.querySelector("nav");
+const kontaktMap = document.querySelector(".Kontakt_map");
+const kontaktBtn = document.querySelector(".Kontakt_btn");
+const kontaktBg = document.querySelector(".Kontakt_bg");
+const counter = document.querySelector(".Ueberuns_counter");
+const badge = document.querySelector(".Ueberuns_badge");
 
 // Burger Navigation
 if (burger && nav) {
-  burger.addEventListener('click', function () {
-    nav.classList.toggle('open');
-    burger.classList.toggle('active');
+  burger.addEventListener("click", function () {
+    nav.classList.toggle("open");
+    burger.classList.toggle("active");
   });
 }
 
 // Global fade-in animation
-document.addEventListener('DOMContentLoaded', function () {
-  const sections = document.querySelectorAll('.fade');
-  const slideLeftElement = document.querySelector('.slide-left');
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".fade");
+  const slideLeftElement = document.querySelector(".slide-left");
 
   const observerOptions = { threshold: 0.2 };
 
   const fadeInOnScroll = (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('fade-in');
+        entry.target.classList.add("fade-in");
         observer.unobserve(entry.target);
       }
     });
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const slideLeftObserver = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('slide-left-in');
+          entry.target.classList.add("slide-left-in");
           observer.unobserve(entry.target);
         }
       });
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // Counter
   if (counter && badge) {
     let count = 0;
-    const target = 25;
+    const target = 30;
     const speed = 100;
 
     const updateCounter = () => {
@@ -75,20 +75,20 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // nav show/hide logic
-document.addEventListener('DOMContentLoaded', function () {
-  const stickyIcon = document.querySelector('.sticky-icon');
+document.addEventListener("DOMContentLoaded", function () {
+  const stickyIcon = document.querySelector(".sticky-icon");
 
   if (nav) {
-    nav.classList.add('hidden'); // Startzustand direkt setzen
+    nav.classList.add("hidden"); // Startzustand direkt setzen
     setTimeout(() => {
-      nav.classList.remove('hidden');
-      nav.classList.add('show');
+      nav.classList.remove("hidden");
+      nav.classList.add("show");
       setupScrollHandling();
     }, 100); // kleine Verzögerung für ein sanftes Einblenden
   }
 
   if (stickyIcon) {
-    stickyIcon.style.opacity = '1';
+    stickyIcon.style.opacity = "1";
   }
 });
 
@@ -96,25 +96,25 @@ document.addEventListener('DOMContentLoaded', function () {
 function setupScrollHandling() {
   let lastScrollTop = 0;
 
-  window.addEventListener('scroll', () => {
-    const nav = document.querySelector('nav');
+  window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
     if (!nav) return;
 
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
-      nav.classList.add('hide');
-      nav.classList.remove('show');
+      nav.classList.add("hide");
+      nav.classList.remove("show");
     } else {
-      nav.classList.add('show');
-      nav.classList.remove('hide');
+      nav.classList.add("show");
+      nav.classList.remove("hide");
     }
     lastScrollTop = Math.max(0, scrollTop);
 
     // Paralax scroll
-    const leistungenSection = document.querySelector('.Leistungen');
+    const leistungenSection = document.querySelector(".Leistungen");
     if (leistungenSection && window.innerWidth > 1022) {
       // Überprüft, ob es sich nicht um ein mobiles Gerät handelt (z.B. Bildschirmbreite > 768px)
-      window.addEventListener('scroll', function () {
+      window.addEventListener("scroll", function () {
         const scrollTop =
           window.pageYOffset || document.documentElement.scrollTop;
         leistungenSection.style.backgroundPositionY = `${scrollTop * 0.5}px`;
@@ -124,10 +124,10 @@ function setupScrollHandling() {
 }
 
 // google btn
-document.querySelector('.Kontakt_map').addEventListener('click', function () {
-  const btn = document.querySelector('.Kontakt_btn');
-  const background = document.querySelector('.Kontakt_bg');
-  background.style.opacity = '0';
-  background.style.pointerEvents = 'none';
-  btn.style.display = 'none';
+document.querySelector(".Kontakt_map").addEventListener("click", function () {
+  const btn = document.querySelector(".Kontakt_btn");
+  const background = document.querySelector(".Kontakt_bg");
+  background.style.opacity = "0";
+  background.style.pointerEvents = "none";
+  btn.style.display = "none";
 });
--- readme.md
+++ readme.md
@@ -9,3 +9,14 @@ https://www.e-recht24.de/
 - Kontakt - Bild ändern zu Weltkarte -- erledigt
 - Facebook raus, Instagram anders darstellen -- halb
 - Alter der Werkstatt einbauen -check1
+
+2 Termin:
+
+- Startbild ändern (Situationsbild - Lenkrad, Weißer Porsche von hinten, mit blur ) -- zu klein - 640 × 427 px
+- 30 Jahre Erfahrungs banner -- erledigt
+- Portfolio: - Gruppierter: - Luftgekühle Motoren (Porsche Diesel (rot), Sonstige, VW Käfer, Porsche 356
+- Kennzeichen bluring
+- save the classics -- erledigt
+- icon hover effekt zoom -- erledigt
+- broken image in gallery -- erledigt
+- zoom image portfolio bei klick
